16865f97cbe374ba6dc2944189024a9a9935ea49
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-09-21 17:11:06 +0200

    Rename params shortcut

--- src/program.js
+++ src/program.js
@@ -2,10 +2,10 @@ const program = require('commander')
 
 program
   .option('-s, --search <search>', 'Search term')
-  .option('-s, --owner <owner>', 'GitHub user or organisation name')
-  .option('-s, --repo <repo>', 'GitHub repository name')
-  .option('-s, --cacheFile <repo>', 'Path to cache file')
-  .option('-b, --fetch', 'Fetch releases from GitHub')
+  .option('-o, --owner <owner>', 'GitHub user or organisation name')
+  .option('-r, --repo <repo>', 'GitHub repository name')
+  .option('-c, --cacheFile <repo>', 'Path to cache file')
+  .option('-f, --fetch', 'Fetch releases from GitHub')
   .parse(process.argv)
 
 module.exports = program
