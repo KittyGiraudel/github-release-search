49af39691f220f5c888a8f0e699918b89e7d7f19
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-09-21 16:06:56 +0200

    Add comment about PER_PAGE constant

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   DEFAULT_CACHE_FILE: './.cache/releases',
+  // `100` appears to be the maximum amount of entries to be returned by the
+  // GitHub API
+  // See: https://mikedeboer.github.io/node-github/#api-repos-getReleases
   PER_PAGE: 100
 }
