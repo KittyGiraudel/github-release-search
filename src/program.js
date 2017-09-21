ac2e7ea494a77c174d6cecabd6e601e7dcc6bf78
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-09-21 17:13:55 +0200

    Add comments to program.js

--- src/program.js
+++ src/program.js
@@ -1,11 +1,17 @@
 const program = require('commander')
 
 program
+  // Fetching and caching
+  .option('-f, --fetch', 'Fetch releases from GitHub')
+  .option('-c, --cacheFile <repo>', 'Path to cache file')
+
+  // Searching
   .option('-s, --search <search>', 'Search term')
+
+  // Project options
   .option('-o, --owner <owner>', 'GitHub user or organisation name')
   .option('-r, --repo <repo>', 'GitHub repository name')
-  .option('-c, --cacheFile <repo>', 'Path to cache file')
-  .option('-f, --fetch', 'Fetch releases from GitHub')
+
   .parse(process.argv)
 
 module.exports = program
